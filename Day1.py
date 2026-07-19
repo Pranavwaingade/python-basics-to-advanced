034da78cfd4d20c40fcaa16bfeb8e3f1d56e0deb
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-19 12:30:43 +0530

    Day1 Python Completed

---
+++ Day1.py
@@ -0,0 +1,74 @@
+# My First Python Program 
+
+print("Hello Mitroo.....!")
+
+# print are used to display output on screen
+
+# -----------------------------------------------------------------------------------------------
+
+# Variables
+
+# name="Pranav"
+# age=21
+# height=5.3
+# course="Python"
+
+# print(name)
+# print(age)
+# print(height)
+# print(course)
+
+# Variable haa data saved karnay sathi used kala jatooo 
+
+# -----------------------------------------------------------------------------------------------
+
+# DataType
+
+# name="Pranav"
+# age=21
+# marks=88.5
+# isStudent=True
+
+# print(type(name))
+# print(type(age))
+# print(type(marks))
+# print(type(isStudent))
+
+# data type mhanjea kay apna konty type cha data store kartoy string , flot,integer , boolean 
+
+
+# -----------------------------------------------------------------------------------------------
+
+# User input
+
+# name=input("Enter the Name:- ")
+# print("Welcome",name)
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# Q1. display the Output 
+
+# print("Yash")
+
+# Q2. data Stored in variables and Display it
+
+# name="Yash"
+# age=21
+# course="Python"
+
+# print(name)
+# print(age)
+# print(course)
+
+
+# Q3. give the User input and display it
+
+# city=input("Enter the City Name:- ")
+# print(city)
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+print("Thanks Learn With me")
+
