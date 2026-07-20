ef693e675edf57b7653faa6190e1a67b09cb962b
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-20 19:17:20 +0530

    Day 2 Complete

---
+++ Day2.py
@@ -0,0 +1,42 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Oprations")
+
+# -----------------------------------------------------------------------------------------------
+
+# a=10
+# b=5
+
+# print(a + b)   # Additon
+# print(a - b)   # Subtraction
+# print(a * b)   # Multiplication
+# print(a / b)   # Division
+# print(a % b)   # Modulus (remainder)
+# print(a ** b)  # Power
+# print(a // b)  # Floor Division
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# Q1.Don numbers user kadun ghya.
+
+# n1=int(input("Enter The First Number:- "))
+# n2=int(input("Enter The Second Number:- "))
+
+# Q2.Tyanchi addition, subtraction, multiplication ani division print kara.
+
+# print("Addition of two Numbers: ",n1 + n2)
+# print("Subtraction of two Numbers: ",n1 - n2)
+# print("Multiplication of two Numbers: ",n1 * n2)
+# print("Division of two Numbers: ",n1 / n2)
+
+
+# Q3. User kadun Don number ghea tyachi Power, Module ,Floor Division kada
+
+# print("Power of the two Number:- ",n1 ** n2)
+# print("Module Of the two Numbers:- ",n1 % n2) 
+# print("Floor Divison of the two Numbers:- ",n1 // n2) 
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
