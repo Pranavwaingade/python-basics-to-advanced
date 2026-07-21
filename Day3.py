b7009ed5bf0a050efeedd6f03688b99a283d7ec2
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-21 19:44:53 +0530

    Day 3 Complete

---
+++ Day3.py
@@ -0,0 +1,84 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Conditions")
+
+# -----------------------------------------------------------------------------------------------
+
+# If  Conditions mule program decision gheto.
+# Example 1
+
+# age = int(input("Enter the Your age:- "))
+# if(age > 18):
+#     print("You Can Vote")
+# else:
+#     print("You Can't Vote")
+
+
+# Python madhe indentation (4 spaces) khup important aahe.
+
+# -----------------------------------------------------------------------------------------------
+
+# Example 2
+
+# num =int(input("Enter the Number:- "))
+# if num%2==0:
+#     print(num,"is Even Number")
+# else:
+#     print(num,"is Odd Number")
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# Q1. Positive, Negative ki Zero?
+
+# num =int(input("Enter the Number:- "))
+# if(num > 0):
+#     print(num,"is Positive")
+# elif(num < 0):
+#     print(num,"is Negative")
+# else:
+#     print(num ,"is Zero")
+
+
+# Q2. Enter the Marks Send the Grade
+
+# marks=int(input("Enter the Marks:- "))
+# if(marks>=95):
+#     print("Grade O")
+# elif(marks >= 90):
+#     print("Grade A")
+# elif(marks >= 80):
+#     print("Grade B")
+# elif(marks >= 60):
+#     print("Grade C")
+# elif(marks >= 35):
+#     print("Grade D")
+# else:
+#     print("YOu Are Failed")
+
+
+# Q3. Find the largest of two numbers.
+
+# num1=int(input("Enter the First Number:-"))
+# num2=int(input("Enter the Second Number:-"))
+
+# if(num1 > num2):
+#     print(num1,"is Greater than",num2)
+# elif(num2 > num1):
+#     print(num2,"is Greater than",num1)
+# else:
+#     print(num1,",",num2,"both are equal")
+
+# Q4 . alredy define pass And user send pass this match then you can login print it
+
+# Password="Pranav@2006"
+# userpass=input("Enter the Password")
+
+# if userpass == Password:
+#     print("Login Succesfully....!")
+# else:
+#     print("Please Check Password")
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
