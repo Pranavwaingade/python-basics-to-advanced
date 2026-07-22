10616eb6f164307f500b1ba0831b324669a4cdb4
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-22 18:57:44 +0530

    Day 4 Completed

---
+++ Day4.py
@@ -0,0 +1,48 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Loops")
+
+# Loop mhanje ekach kaam punha-punha karne.
+
+# -----------------------------------------------------------------------------------------------
+
+# 1. For loop
+
+# Print the number 1 to 5
+
+# for i in range(1,6):
+#     print(i)
+
+
+# Print the number 1 to 11
+
+# for i in range(1,11):
+#     print(i)
+
+# Addtion of 1 to 10 numbers
+
+# sum=0
+# for i in range(1,11):
+#     sum=sum+i
+# print("1 to 10 number sum is",sum)
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# 1. 1 te 20 paryant numbers print kara.
+
+# for i in range(1,21):
+#     print(i)
+
+# 2. 10 te 1 reverse order madhye print kara.
+# for i in range(10,0,-1):
+#     print(i)
+
+# 3. enter user input print 5 times
+
+# name=input("Enter the name:- ")
+# count=int(input("How Many Time Print it:- "))
+# for i in range(1,count+1):
+#     print(name)
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
