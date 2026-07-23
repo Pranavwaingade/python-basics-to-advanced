8807803f0ab665dd1f2685626889eff64fbc58bc
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-23 21:28:56 +0530

    Day 5 Complete

---
+++ Day5.py
@@ -0,0 +1,52 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Loops")
+
+# -----------------------------------------------------------------------------------------------
+# 2. While loop 
+# while loop condition True asel toparyant chalto.
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+
+# 1 te 10 numbers print kara.
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# 10 te 1 reverse print kara.
+
+# i=10
+# while i >0:
+#     print(i)
+#     i-=1
+
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+# Q1. User kadun ek number ghya ani 1 te 10 paryant tyacha table print kara.
+
+# num=int(input("Enter the Number:- "))
+# i=1
+# while i <=10:
+#     print(num,"X",i,"=",num*i)
+#     i+=1
+
+
+# Q2. User kadun password ghya. Jo paryant password "python123" nasel to paryant punha input ghya.
+
+# password=""
+# while password != "python123":
+#     password=input("Enter the Password:- ")
+#     if password != "python123":
+#         print("Please try again")
+# print("Login Succesfully")
+
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
