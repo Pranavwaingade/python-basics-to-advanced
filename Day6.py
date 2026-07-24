2e2cb20bf0f5f1180cd48b18cf4abb75a35324db
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-24 18:41:48 +0530

    Day 6 Completed

---
+++ Day6.py
@@ -0,0 +1,110 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Functions")
+
+# -----------------------------------------------------------------------------------------------
+# Functions
+#            Function mhanje ek block of code jo garaj padli ki punha-punha vaparta yeto.
+
+#Example 1:
+
+# def greet():
+#     print("Hello Welcome jiiii.....!")
+
+# greet()
+
+
+# Example 2:              (here Used Are Parameters)
+
+# def greet(name):
+#     print("Hello",name ,"jiii......!")
+
+# greet("Pranav")
+# greet("Yash")
+
+# Example 3:              (here Used Are return)
+
+# def add(a,b):
+#     return a+b
+# result =add(5,3)
+# print(result)
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# 1. Square Function
+
+# def Square(num):
+#     return num*num
+
+# num=int(input("Enter the Number:- "))
+# result=Square(num)
+# print(result)
+
+# 2. Largest Number
+# Don numbers ghya ani function vaprun motha number print kara.
+
+# def largest(n1,n2):
+#     if(n1 > n2):
+#         print(n1,"is Largest than",n2)
+#     elif(n1 < n2):
+#         print(n2,"is Largest than",n1)
+#     else:
+#         print(n1,n2,"Both numbers Are Equal")
+
+# n1=int(input("Enter the First Number:- "))
+# n2=int(input("Enter the Second Number:- "))
+# largest(n1,n2)
+
+
+
+#3.Greeting Function
+# User kadun nav ghya ani function madhun print kara.
+
+# def greek(name):
+#     print("Hello",name,"Welcome TO Python..!")
+
+# name=input("Enter the Name:- ")
+# greek(name)
+
+
+#-------------------------------------- Challenge ---------------------------------------------------
+
+# Calculator function banav:
+# 1. Add 2. Subtract 3. Multiply 4. Divide
+# User choice ghe ani function vaprun operation kara.
+
+# def Add(a,b):
+#     return a+b
+# def Sub(a,b):
+#     return a-b
+# def Pro(a,b):
+#     return a*b
+# def Div(a,b):
+#     return a/b
+
+# def calculator():
+#     oprations=input("Which Operation YOu Perform(Add,Sum,Pro,Div):-  ")
+#     if(oprations == "Add"):
+#         n1=int(input("Enter The First Number "))
+#         n2=int(input("Enter The Second Number "))
+#         print("Addition of two number is",Add(n1,n2))
+#     elif(oprations == "Sub"):
+#         n1=int(input("Enter The First Number "))
+#         n2=int(input("Enter The Second Number "))
+#         print("Subtraction of two number is",Sub(n1,n2))
+#     elif(oprations == "Pro"):
+#         n1=int(input("Enter The First Number "))
+#         n2=int(input("Enter The Second Number "))
+#         print("Product of two number is",Pro(n1,n2))
+#     elif(oprations == "Div"):
+#         n1=int(input("Enter The First Number "))
+#         n2=int(input("Enter The Second Number "))
+#         print("Division of two number is",Div(n1,n2))
+#     else:
+#         print("Give me Valid Oprations")
+
+# calculator()
+    
+    
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
