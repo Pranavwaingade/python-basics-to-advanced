62b4828b598a909cbcd57dcf0a2706f456fcbf6a
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-25 18:06:28 +0530

    Day 7 Completed

---
+++ Day7.py
@@ -0,0 +1,84 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Lists")
+
+# -----------------------------------------------------------------------------------------------
+# Lists
+#           List mhanje eka variable madhye khup values store karta yetat.
+
+#Example 1:
+
+# fruits =['Apple','Banana','Mango']
+# print(fruits)
+
+#Example 2: Only One item access karya cha
+
+# fruits =['Apple','Banana','Mango']
+
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+
+# Example 3: Add Item
+
+# fruits =['Apple','Banana','Mango']
+
+# fruits.append('Orange')
+# print(fruits)
+
+#Example 4: remove Item
+
+# fruits =['Apple','Banana','Mango']
+
+# fruits.remove('Apple')
+# print(fruits)
+
+#Example 5: Loop Through item
+
+# fruits =['Apple','Banana','Mango']
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# 1.5 favorite games chi list banva ani print kara.
+
+# Games=['Cricket','Football','Boxing']
+# print(Games)
+
+# 2.List madhye ek navin game append() kara ani print kara.
+
+# Games.append('Badminton')
+# print(Games)
+
+# 3.Ek game remove() kara ani updated list print kara.
+
+# Games.remove('Football')
+# print(Games)
+
+# 4.for loop vaprun list madhle sagle games ek-ek line var print kara.
+
+# for game in Games:
+#     print(game)
+
+
+#-------------------------------------- Challenge ---------------------------------------------------
+
+# Numbers chi list ghya:
+# for loop vaprun saglya numbers chi sum calculate kara.
+
+# sum=0
+# numbers=[10,20,30,40,50,60,70,80,90,100]
+
+# for number in numbers:
+#     sum=sum+number
+
+# print("Total Sum is",sum)
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
