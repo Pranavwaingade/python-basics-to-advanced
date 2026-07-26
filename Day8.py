c491e048fbae139356616f68a63c7233d95d33f6
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-26 17:44:09 +0530

    Day 8 Complete

---
+++ Day8.py
@@ -0,0 +1,66 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Dictionaries")
+
+# -----------------------------------------------------------------------------------------------
+# Dictionaries
+#               Dictionary madhye data key : value pair madhye store hoto.
+
+# Example 1:
+
+# student ={ "name":"Pranav","age":21,"City":"Pune"}
+# print(student)
+
+# Example 2 - Value Access
+
+# student={"name":"Bhakti","age":21,"City":"Pune"}
+# print(student["name"])
+# print(student["age"])
+# print(student["City"])
+
+
+# Example 3 - Add New Item
+
+# student={"name":"Bhakti","age":21,"City":"Pune"}
+# student["Course"]="Python"
+# print(student)
+
+# Example 4 - Loop Through Dictionary
+
+# student={"name":"Pranav","age":21,"City":"Pune","Course":"Python"}
+
+# for key ,value in student.items():
+#     print(key,":",value)
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# 1. student navacha dictionary banva:  name ,age ,city
+
+# student={"name":"john","age":18,"city":"Pune"}
+# print(student)
+
+# 2. course navachi new key add kara.
+# student["course"]="Java Develpor"
+# print(student)
+
+# 3.for loop vaprun saglya key ani value print kara.
+
+# for key,value in student.items():
+#     print(key,":",value)
+
+
+#-------------------------------------- Challenge ---------------------------------------------------
+
+# Q1. 3 students cha data list + dictionary madhye store kara.
+
+# Students=[{"name":"Pranav","age":21,"Course":"Mern Stack"},
+#           {"name":"Bhaktu","age":21,"Course":"Data Analytics"},
+#           {"name":"Sweeti","age":21,"Course":"Testing"}]
+
+# for student in Students:
+#     print(student["name"],":-",student["Course"])
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
