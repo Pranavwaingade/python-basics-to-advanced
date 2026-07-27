5b39700d2dd4c2b6f65619dcdeb2ccd510e76e65
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-27 20:34:21 +0530

    Day 9 Completed

---
+++ Day9.py
@@ -0,0 +1,102 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota OOP")
+
+# -----------------------------------------------------------------------------------------------
+# Object-Oriented Programming
+#                    OOP Python madhla khup important topic aahe. Interviews madhye pan nehemi vichartat.
+
+#  Class & Object
+#                 Class mhanje blueprint, Object mhanje tyach blueprint pasun banlela real object.
+
+# Example 1
+
+# class student:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+    
+# student1=student("Pranav",21)
+
+# print(student1.name)
+# print(student1.age)
+
+
+# Methods
+
+# class student:
+#     def __init__(self,name):
+#         self.name=name
+        
+#     def greet(self):
+#         print("Hello",self.name)
+
+# student=student("Pranav")
+# student.greet()
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+# 1. Car navachi class banva.
+# Properties: brand,    model
+
+# class car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+    
+#     def display(self):
+#         print("Brand:- ",self.brand)
+#         print("Model:- ",self.model)
+
+# my_car = car("Tata","NEXON")
+# my_car.display()
+
+# -----------------------------------------------------------------------------------------------
+
+# 2.Person class banva.
+# Properties: name  age
+
+# class Person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+#     def display(self):
+#         print("Name:-",self.name)
+#         print("Age:-",self.age)
+
+# person1=Person("Pranav",21)
+# person1.display()
+
+
+#-------------------------------------- Challenge ---------------------------------------------------
+
+# Calculator class banva.
+# Methods: add() ,subtract() ,multiply() ,divide()
+# Object create kara ani sagle methods call kara.
+
+
+# class Calculator:
+#     def add(self,a,b):
+#         return a+b
+    
+#     def sub(self,a,b):
+#         return a-b
+    
+#     def pro(self,a,b):
+#         return a*b
+    
+#     def div(self,a,b):
+#         return a/b
+    
+# calculation1=Calculator()
+
+# print("Addition:-",calculation1.add(10,5))
+# print("Subtraction:-",calculation1.sub(10,5))
+# print("Multiplication:-",calculation1.pro(10,5))
+# print("DiviSion:- ",calculation1.div(10,5))
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
