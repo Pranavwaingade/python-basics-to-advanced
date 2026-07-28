2e7373fc1cabf72f92cb0a972b3e9fc226d4a134
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-28 18:34:46 +0530

    Day 10 Completed

---
+++ Day10.py
@@ -0,0 +1,50 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota File Handling")
+
+# -----------------------------------------------------------------------------------------------
+# File Handling
+#              File madhye data save ani read karayla File Handling vaparto.
+
+# Write File
+# file=open("Day10.txt","w")
+# file.write("Hello Pranav")
+# file.close()
+
+# Ṛead file
+# file=open("Day10.txt","r")
+# print(file.read())
+# file.close()
+
+# Best Meathod
+
+# with open("Day10.txt","a") as file:
+#     file.write("\nWelcome Pranav")
+
+# with open("Day10.txt","r") as file:
+#     print(file.read())
+
+
+# ---------------------------------- Home Work Section -------------------------------------------
+
+#  1. notes.txt navachi file create kara ani tyat "Hello Python" write kara.
+#  2. Tyach file madhla data read karun print kara.
+#  3. append mode ("a") vaprun "I love coding" add kara.
+#  4. Punha file read karun complete content print kara.
+
+
+# with open("Day10notes.txt","w") as file:
+#     file.write("Hello Python")
+
+# with open("Day10notes.txt","r") as file:
+#     print(file.read())
+
+# with open("Day10notes.txt","a") as file:
+#     file.write("\n I Love YOu Python")
+
+# with open("Day10notes.txt","r") as file:
+#     print(file.read())
+
+
+# -------------------------------------- Thank you ---------------------------------------------------
+
+# print("Thanks Learn With me")
