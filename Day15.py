4df914a42eda09d7666fb6a737db720a49d1f9a4
Pranav Waingade <pranavwaingade3@gmail.com>
2026-08-02 18:01:44 +0530

    Day 15 Completed

---
+++ Day15.py
@@ -0,0 +1,85 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota Modules & Packages")
+
+# ---------------------------------------- Part 1 --------------------------------------------------------------------------------
+# Modules
+#          Module mhanje Python file (.py) jyat functions, variables, classes astat. Tyanna dusrya file madhye reuse karta yeta.
+
+# import Day15math as m
+# print(m.add(5,10))
+
+# print(m.sub(15,10))
+
+# print(m.pro(5,10))
+
+# print(m.div(10,5))
+
+# 1. import
+
+# import math
+# print(math.sqrt(25))
+
+# 2. from ... import
+
+# from math import sqrt
+# print(sqrt(49))
+
+
+# 3. Alias (as)
+
+# import math as m
+# print(m.pi)
+
+# 4.Built-in Modules
+# Random
+
+# import random
+# print(random.randint(1,10))
+
+# Datetime
+
+# from datetime import datetime
+# print(datetime.now())
+
+
+# Math
+
+# import math
+# print(math.factorial(5))
+# print(math.pi)
+# print(math.sqrt(64))
+
+# ---------------------------------------- Part 2 ---------------------------------------------------------------------------------
+
+# Package = Modules cha folder.
+
+# mypackage/
+# │
+# ├── calculator.py
+# ├── student.py
+# └── __init__.py
+
+# ---------------------------------- Home Work Section ----------------------------------------------------------------------------
+
+# import math
+
+# print(math.sqrt(100))
+
+
+# print(math.factorial(6))
+
+# import random
+# print(random.randint(1,100))
+
+# from datetime import datetime
+# print(datetime.now())
+
+
+# import Day15math as m
+# print(m.add(10,5))
+# print(m.sub(15,5))
+# print(m.pro(15,5))
+# print(m.div(15,5))
+
+# -------------------------------------- Thank you --------------------------------------------------------------------------------
+# print("Thanks Learn With me")
