48bb70ea492cfacbbeb14e804bb47b723c0c9829
Pranav Waingade <pranavwaingade3@gmail.com>
2026-08-05 17:49:04 +0530

    Day 16 Completed

---
+++ Day16.py
@@ -0,0 +1,66 @@
+print("Hello Mitrooooo.....!")
+print("Tar Ajj Apna Shiknar Ahota List Comprehension")
+
+# ---------------------------------------- Part 1 --------------------------------------------------------------------------------
+# List Comprehension
+#                     List Comprehension hi list banvnyachi short ani Pythonic way aahe.
+
+# number = [i for i in range(1,6)]
+# print(number)
+
+
+# Square of Numbers
+# Square=[i*i for i in range(1,6)]
+# print(Square)
+
+
+# Condition in List Comprehension
+
+# Fakta even numbers:
+# even=[i for i in range(1,11) if i % 2 == 0]
+# print(even)
+
+# Strings
+# Word="Pranav"
+# letters=[ch for ch in Word]
+# print(letters)
+
+
+# ---------------------------------- Home Work Section ----------------------------------------------------------------------------
+
+# 1. te 10 numbers chi list List Comprehension ne banva.
+# 2. te 10 squares chi list banva.
+
+# number=[ i for i in range(1,11)]
+# print(number)
+
+# number=[i*i for i in range(1,11)]
+# print(number)
+
+
+# 1 te 20 madhle even numbers chi list banva.
+# 1 te 20 madhle odd numbers chi list banva.
+
+# even=[ i for i in range(1,21) if i % 2 == 0]
+# print(even)
+
+# odd=[ i for i in range(1,21) if i % 2 != 0]
+# print(odd)
+
+
+# User kadun ek word ghya ani tyachi characters list madhye convert kara.
+
+# user_input = input("Enter the String:- ")
+# letter = [ ch for ch in user_input]
+# print(letter)
+
+#-------------------------------------- Challenge ---------------------------------------------------
+
+# List Comprehension vaprun fakta 20 peksha mothya numbers chi new list banva.
+
+# numbers = [10, 15, 20, 25, 30]
+# user_requriment=[number for number in numbers if number > 20]
+# print("20 Graters Numbers:- ",user_requriment)
+
+# -------------------------------------- Thank you --------------------------------------------------------------------------------
+# print("Thanks Learn With me")
